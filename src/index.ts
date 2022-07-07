b090efdd2b03040ef225bfd58c08e35067221e4b
boedegoat <bhremada.fka@gmail.com>
2022-07-07 11:25:17 +0700

    add bot invite path

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,14 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({
     app: 'Tebo Discord Bot',
+    inviteToServer: 'https://tebo-discord-bot.herokuapp.com/invite',
     version: process.env.npm_package_version,
     author: 'https://github.com/boedegoat',
   });
+});
+
+app.get('/invite', (req, res) => {
+  res.redirect('https://discord.com/api/oauth2/authorize?client_id=863731372572934145&permissions=8&scope=bot%20applications.commands');
 });
 
 const port = process.env.PORT || 5000;
