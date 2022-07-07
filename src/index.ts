e526300fbd82dda84d1002bdc19fec9fdad9c991
boedegoat <bhremada.fka@gmail.com>
2022-07-07 09:08:34 +0700

    add web server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from 'discord.js';
+import express from 'express';
 import onInteraction from './events/onInteraction';
 import onReady from './events/onReady';
 
@@ -15,3 +16,17 @@ bot.on('ready', onReady);
 bot.on('interactionCreate', onInteraction);
 
 bot.login(process.env.TOKEN);
+
+// web server
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    app: 'Tebo Discord Bot',
+    version: process.env.npm_package_version,
+    author: 'https://github.com/boedegoat',
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
