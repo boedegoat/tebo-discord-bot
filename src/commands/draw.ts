352efdeee975718692453a96bd9605245805fa89
boedegoat <bhremada.fka@gmail.com>
2022-07-13 16:37:51 +0700

    add draw command

---
+++ src/commands/draw.ts
@@ -0,0 +1,71 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import axios from 'axios';
+// @ts-ignore
+import createCollage from '@settlin/collage';
+// @ts-ignore
+import Frame from 'canvas-to-buffer';
+import { MessageAttachment } from 'discord.js';
+import Command from '../interfaces/Command';
+import createEmbed from '../lib/createEmbed';
+
+const draw: Command = {
+  data: new SlashCommandBuilder()
+    .setName('draw')
+    .setDescription('Change any text to images by using AI 🎨')
+    .addStringOption((option) => option
+      .setName('text')
+      .setDescription('What do you want to see ? (e.g. a coffee shop logo that has a colorful unicorn drinking a coffee)')
+      .setRequired(true)),
+  run: async (interaction) => {
+    const { options } = interaction;
+
+    const text = options.getString('text', true);
+
+    const drawEmbed = createEmbed();
+    drawEmbed.setAuthor({
+      name: '🎨 Drawing',
+    });
+    drawEmbed.setTitle(text);
+    drawEmbed.setDescription('The more specific your text, the longer AI will take to draw, please be patient 👍');
+
+    await interaction.reply({ embeds: [drawEmbed] });
+
+    const startTime = Date.now();
+    const { data } = await axios.post('https://bf.dallemini.ai/generate', {
+      prompt: text,
+    });
+    const finishTime = Math.round((Date.now() - startTime) / 1000); // second
+
+    // change image results to buffer
+    const imageBuffers = (data.images as string[]).map((image) => Buffer.from(image, 'base64'));
+
+    // create collage to combine image results
+    const canvas = await createCollage({
+      width: 3,
+      height: 3,
+      imageWidth: 300,
+      imageHeight: 300,
+      spacing: 20,
+      sources: imageBuffers,
+    });
+
+    // create buffer from created canvas collage
+    const canvasBuffer = new Frame(canvas).toBuffer();
+    // then attach it
+    const attachment = new MessageAttachment(canvasBuffer, `${text.replace(/ /g, '-')}.png`);
+
+    const finishEmbed = createEmbed();
+    finishEmbed.setAuthor({
+      name: `✅ Drawing Finished in ${finishTime} seconds`,
+    });
+    finishEmbed.setTitle(text);
+    finishEmbed.setDescription(`Requested by ${interaction.user}`);
+
+    await interaction.editReply({
+      embeds: [finishEmbed],
+      files: [attachment],
+    });
+  },
+};
+
+export default draw;
--- src/commands/help.ts
+++ src/commands/help.ts
@@ -9,9 +9,9 @@ const help: Command = {
     .setDescription('Provides guides for using this bot'),
   run: async (interaction) => {
     const embed = createEmbed();
-    embed.setTitle(`Guides (${commands.length} commands)`);
+    embed.setTitle(`Guides (${commands.length - 1} commands)`);
     embed.setDescription('💡 tip:\nuse **arrow keys** for selecting different commands\nuse **tab** for selecting command parameters');
-    embed.addFields(commands.map((command) => ({
+    embed.addFields(commands.filter((command) => command.data.name !== 'help').map((command) => ({
       name: `/${command.data.name}`,
       value: command.data.description,
     })));
