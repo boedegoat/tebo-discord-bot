3de68dcd05c75e0ce09c70bec376702152279362
boedegoat <bhremada.fka@gmail.com>
2022-07-17 18:28:55 +0700

    edit intents

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import runWebServer from './server';
 
 // eslint-disable-next-line import/prefer-default-export
 export const bot = new Client({
-  intents: ['GUILDS', 'GUILD_VOICE_STATES', 'GUILD_MEMBERS'], // tell Discord what events your bot should receive.
+  intents: ['GUILDS', 'GUILD_VOICE_STATES'], // tell Discord what events your bot should receive.
 });
 
 bot.on('ready', onReady);
