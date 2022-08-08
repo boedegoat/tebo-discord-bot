abbc1f5506e93b62f25e60c31042b7afa45f22e7
Bhremada Fevreano <bhremada.fka@gmail.com>
2022-08-08 11:36:14 +0000

    add nodemon to dev deps

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+    deps = [
+        pkgs.python37
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-16_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+    ];
+}
